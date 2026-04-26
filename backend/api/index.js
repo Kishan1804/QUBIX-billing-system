a980e273a81c88050e904254ae275503c40b29a8
Kishan Patel <patelkishan9879@gmail.com>
2026-04-26 11:03:04 +0530

    fix: db connection

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,13 @@
 import {app} from '../src/app.js'
+import connectDB from '../src/db/index.js'
 
-export default app
+let isConnected = false
+
+export default async function handler(req, res) {
+    if(!isConnected) {
+        await connectDB()
+        isConnected = true
+    }
+
+    return app(req, res)
+}
