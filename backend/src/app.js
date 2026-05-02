597b0a0e10978f995c9d67db60b85e97622d4097
Kishan Patel <patelkishan9879@gmail.com>
2026-05-02 13:17:50 +0530

    add: cors PATCH method

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
 const corsOptions = {
     origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }
 
